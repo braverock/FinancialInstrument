a6dc28e76fae1b856dd560dcde57ef83c5fd8c90
Brian G. Peterson <brian@braverock.com>
2010-01-19 14:58:00 +0000

    - add comments
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/FinancialInstrument@184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/demo.R
+++ demo/demo.R
@@ -1,12 +1,26 @@
 require(FinancialInstrument)
+
+# currencies need to be defined first
+
 currency("USD")
 currency("GBP")
 currency("EUR")
 currency("YEN")
+
+# now exchange rates
 exchange_rate("USDGBP","USD","GBP")
+exchange_rate("USDEUR","USD","EUR")
 exchange_rate("EURGBP","EUR","GBP")
 exchange_rate("EURYEN","EUR","YEN")
 exchange_rate("USDYEN","USD","YEN")
+
+# now a stock and an option contract on it
 stock("IBM","USD",1)
 option(".IBM","USD",100,underlying_id="IBM")
-# Jeff to pull put and call option series for 2009
+#@TODO:Jeff to pull put and call option series for 2009
+
+# bond & bond future
+
+# non-US
+
+# add a spread
