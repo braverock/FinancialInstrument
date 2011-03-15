752a4617b0390a0f283c8304133831011e0032f2
Brian G. Peterson <brian@braverock.com>
2011-03-15 14:31:03 +0000

    - fix syntax error in is.list check
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/FinancialInstrument@577 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/instrument.R
+++ R/instrument.R
@@ -97,7 +97,9 @@ instrument<-function(primary_id , ..., currency , multiplier , tick_size=NULL, i
           targ<-arg[['...']]
           arg[['...']]<-NULL
           arg<-c(arg,targ)
-  } 
+      }
+  }
+  
   #check for identifiers we recognize 
   ident_str<-c("X.RIC","RIC","CUSIP","SEDOL","OSI","Bloomberg","Reuters","ISIN","CQG","TT","Yahoo","Google")
   for(i_s in ident_str ){
@@ -112,7 +114,7 @@ instrument<-function(primary_id , ..., currency , multiplier , tick_size=NULL, i
   
   ## TODO note that multiplier could be a time series, probably add code here to check
   if(!is.numeric(multiplier) | length(multiplier) > 1) stop("multiplier must be a single number")
-  if(!is.null(tick_size) && !is.numeric(tick_size) | length(tick_size) > 1) stop("tick_size must be NULL or a single number")
+  if(!is.null(tick_size) && (!is.numeric(tick_size) | length(tick_size) > 1)) stop("tick_size must be NULL or a single number")
   
   if(is.null(type)) tclass="instrument" else tclass = c(type,"instrument")
 
