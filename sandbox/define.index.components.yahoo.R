737e7e1bb46bf63ba131b3b5432760755c995905
Garrett See <gsee000@gmail.com>
2011-10-05 17:00:44 +0000

    updates to define_components.yahoo
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/FinancialInstrument@813 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/define.index.components.yahoo.R
+++ sandbox/define.index.components.yahoo.R
@@ -2,7 +2,7 @@
 #' Define an Index and it's components using yahoo
 #'
 #' Get the components of an index and define instruments
-#' @param Symbol character yahoo ticker symbol for a stock index (e.g. "^DJI" or "^GDAX")
+#' @param Symbol character yahoo ticker symbol for a stock index (e.g. "^DJI" or "^GDAXI")
 #' @param currency
 #' @return called for side-effect, but it will return a list with 2 components:
 #' \item{synthetic}{name of the \code{\link{synthetic}} instrument that was defined to hold the metadata of the index.} 
@@ -22,10 +22,17 @@ define_components.yahoo <- function(Symbol, currency) {
     mdata <- x[which.max(sapply(x, NROW))][[1]][,1:2]
     new.Symbol <- synthetic(Symbol, ccy, members=paste(mdata[,1]), src=list(src='yahoo',name=Symbol), identifiers=list(yahoo=Symbol))
     if (!identical(integer(0), grep("There is no Components data", mdata[,1]))) stop("No Components Data Available for ", Symbol)
+    stks <- rep(NA_character_, NROW(mdata))    
     for (i in 1:NROW(mdata)) {
-        stock(paste(mdata[i,1]), currency=ccy, Name=paste(mdata[i,2]), member.of=new.Symbol)
+        tmpsym <- paste(mdata[i,1])        
+        stks[i] <- stock(tmpsym, currency=ccy, Name=paste(mdata[i,2]), member.of=new.Symbol)
+        if (!identical(tmpsym,stks[i])) { 
+            # add info about how to find data and metadata
+            instrument_attr(stks[i], 'src', list(src='yahoo', name=tmpsym))           
+            instrument_attr(stks[i], 'identifiers', list(yahoo=tmpsym)) 
+        } 
     }
-    list(synthetic=new.Symbol, stock=paste(mdata[,1]))
+    list(synthetic=new.Symbol, stock=stks)
 }
 
 #define_components.yahoo('^STOXX50E','EUR')
@@ -35,4 +42,14 @@ define_components.yahoo <- function(Symbol, currency) {
 #memb5 <- getInstrument(instr$members[5])
 #memb5
 #getInstrument(memb5$member.of)
+
+
+#define_components.yahoo("^GDAXI","EUR")
+#getSymbols("X63DU.DE")
+#getSymbols("63DU.DE")
+#getInstrument("X63DU.DE")
+#getInstrument("63DU.DE")
+
+
+
 
